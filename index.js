95ed3b1a7347111c52ac0dbdd57df9434346d5d2
Nicholas <Nicholas>
2017-04-23 14:55:49 -0500

    Initial Load

---
+++ index.js
@@ -0,0 +1,76 @@
+
+var i;
+
+
+function renderDate() {
+
+  var d = new Date();
+  var day = d.getDate();
+  var month = (d.getMonth() + 1);
+  var year = d.getFullYear();
+  if(day < 10) {
+    day = "0" + day;
+  }
+  var date = day + '/' + month + '/' + year;
+  renderTime();
+  document.getElementById("date").innerHTML = date;
+  setTimeout(renderTime, 500);
+}
+
+function renderTime() {
+  var t = new Date();
+  var hours = t.getHours();
+  var minutes = t.getMinutes();
+  var seconds = t.getSeconds();
+  var pm = false;
+  var greeting = "Good Morning";
+
+  if(hours > 0 && hours < 12) {
+    greeting = "Good Morning";
+  } else if(hours >= 12 && hours < 19) {
+    greeting = "Good Afternoon";
+  } else {
+    greeting = "Good Evening";
+  }
+
+  if(hours >= 12) {
+    pm = true;
+    hours -= 12;
+  }
+
+  if(i === undefined) {
+    i = 0;
+  }
+//  if(seconds == 0 && (minutes == 0 || minutes == 30)) {
+  if((seconds == 0 || seconds == 1) /*&& (minutes % 2) == 0*/) {
+    //alert("PING!");
+    document.getElementById("weatherTest1").innerHTML = i;
+    i++;
+  }
+//  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  } else {
+    hours = hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  } else {
+    minutes = minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  } else {
+    seconds = seconds;
+  }
+  //alert(hours + "--" + minutes + "--" + seconds);
+  var ToD = "am";
+  if (pm) {
+    ToD = "pm";
+  }
+  var time = hours + ':' + minutes + ToD;
+  $("tile2").append("TEST TEXT");
+  document.getElementById("time").innerHTML = time;
+  document.getElementById("greet").innerHTML = greeting;
+  setTimeout(renderDate, 500);
+}
---
+++ weather.js
@@ -0,0 +1,29 @@
+//import DarkSkyApi from '/node_modules/dark-sky-api/src/dark-sky-api';
+
+function renderWeather() {
+  //DarkSkyApi.apiKey = 'f03fd58a03d74bb2965c7d04439d8687';
+  //DarkSkyApi.loadCurrent()
+  //  .then(result => console.log(result));
+  var t = new Date();
+  var hours = t.getHours();
+  var minutes = t.getMinutes();
+  var seconds = t.getSeconds();
+  var title = "Weather";
+  var testing = "Pass" + hours + ":" + minutes + ":" + seconds;
+  var counter = 0;
+  if((seconds == 1) && ((minutes % 15) == 0)) {
+    document.getElementById("weatherTest" + counter).innerHTML = testing;
+    counter++;
+    //alert("I'm working sorta");
+  }
+  document.getElementById("column1").innerHTML = title;
+  if(counter >= 4) {
+    counter = 0;
+  }
+  //getWeather();
+  setTimeout(weatherCycle,500);
+}
+
+function weatherCycle() {
+  renderWeather();
+}
